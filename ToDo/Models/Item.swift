d52a09c1232131bd09513ecb5f242816b6def71b
sariyya.abdullayeva <sariyya.abdullayeva@gmail.com>
2025-02-11 10:56:32 +0200

    Add Item model

--- ToDo/Models/Item.swift
+++ ToDo/Models/Item.swift
@@ -5,3 +5,14 @@
 //  Created by Sariyya Abdullayeva on 10.02.2025.
 //
 
+import Foundation
+
+class Item: Codable {
+    var title: String
+    var done: Bool
+    
+    init(title: String, done: Bool = false) {
+        self.title = title
+        self.done = done
+    }
+}
