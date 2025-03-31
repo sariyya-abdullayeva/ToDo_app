2c7bfcc1640bb1ebc66535c4eb20936f0befe8df
Sariyya Abdullah <work@Sariyyas-MacBook-Pro.local>
2025-03-31 20:11:07 +0300

    Add font and soft UI color assets for theming

---
+++ ToDo/Views/CategoryGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryGridCell.swift
+//  ToDo
+//
+//  Created by Sariyya Abdullayeva on 07.03.2025.
+//
+
+import UIKit
+
+class CategoryGridCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ ToDo/Views/NavigationBarHelper.swift
@@ -0,0 +1,7 @@
+//
+//  NavigationBarHelper.swift
+//  ToDo
+//
+//  Created by Sariyya Abdullayeva on 20.02.2025.
+//
+
